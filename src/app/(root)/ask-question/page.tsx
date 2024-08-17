90b7762a8f5a1698bad20af44e88de2e7620b82e
Kunal Singh <ks820177@gmail.com>
2024-08-17 16:09:17 +0530

    refactored ask-question page and QuestionCard component according to clerk

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -5,8 +5,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 async function page() {
-  // const { userId } = auth();
-  const userId = "123456";
+  const { userId } = auth();
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
--- src/components/cards/QuestionCard.tsx
+++ src/components/cards/QuestionCard.tsx
@@ -48,7 +48,7 @@ function QuestionCard({
       </div>
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          imgUrl={"/assets/icons/avatar.svg"}
+          imgUrl={author.picture}
           alt={"user"}
           value={author.name}
           title={` - asked ${getTimeStamp(createdAt)}`}
