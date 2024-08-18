014c98c696f9bf0967232f4846fa6671620f46f9
Kunal Singh <ks820177@gmail.com>
2024-08-18 19:50:28 +0530

    added tag pages

--- src/app/(root)/tags/page.tsx
+++ src/app/(root)/tags/page.tsx
@@ -1,5 +1,70 @@
-function page() {
-  return <div>page</div>;
+import UserCard from "@/components/cards/UserCard";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { UserFilters } from "@/constants/filter";
+import { getAllTags } from "@/lib/actions/tag.action";
+import Link from "next/link";
+
+async function page() {
+  const result = await getAllTags({});
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Tags</h1>
+        {/* <Link href={"/ask-question"} className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !test-light-900">
+            Ask a Question
+          </Button>
+        </Link> */}
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route={"/tags"}
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for tags"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={UserFilters}
+          otherClass="min-h-[56px] sm:min-w-[176px]"
+        />
+      </div>
+      <section className="mt-12 flex flex-wrap gap-4">
+        {result && result?.tags.length > 0 ? (
+          result?.tags.map((tag) => (
+            <Link
+              key={tag._id}
+              href={`/tags/${tag._id}`}
+              className="shadow-light100_darknone"
+            >
+              <div className="flex flex-col w-full items-center justify-center px-8 py-10 sm:w-[260px] background_light900_dark200 light-border border rounded-2xl">
+                <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+                  <p className="paragraph-semibold text-dark300_light900">
+                    {tag.name}
+                  </p>
+                </div>
+                <p className="small-medium text-dark400_light500 mt-3.5">
+                  <span className="body-semibold primary-text-gradient mr-2.5">
+                    {tag.questions.length}+
+                  </span>
+                  Questions
+                </p>
+              </div>
+            </Link>
+          ))
+        ) : (
+          <NoResult
+            title="No Tags Found"
+            description="It looks like there are no tags yet"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </section>
+    </>
+  );
 }
 
 export default page;
--- src/lib/actions/tag.action.ts
+++ src/lib/actions/tag.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.type";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.type";
+import Tag from "@/database/tag.model";
 
 export async function getInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -21,6 +22,22 @@ export async function getInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "tag2" },
       { _id: "3", name: "tag3" },
     ];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+    const { page = 1, pageSize = 20, filter, searchQuery } = params;
+    const tags = await Tag.find({});
+
+    if (!tags) {
+      throw new Error("Tags not found");
+    }
+
+    return { tags };
   } catch (error) {
     console.log(error);
   }
